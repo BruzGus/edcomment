4e16f9bec8b3dcf2be983e3ac7fc99ca1af605c3
Bryan Agusto Cruz Castro <bryan.cc.652@gmail.com>
2020-02-08 21:51:05 -0400

    incoporando a los modelos: Comment, Vote y refactorizando los ya existentes.

--- migration/migration.go
+++ migration/migration.go
@@ -2,10 +2,13 @@ package migration
 
 import (
 	"github.com/BruzGus/edcomment/configuration"
+	"github.com/BruzGus/edcomment/models"
 )
 
 func Migrate() {
 	db := configuration.GetConnection()
 	defer db.Close()
+
+	db.CreateTable(models.User{})
 
 }
--- models/user.go
+++ models/user.go
@@ -2,13 +2,14 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+// User ..., Modelo usuario para el formatear la DATA
 type User struct {
 	gorm.Model
-	Userame     string    `json: "username" gorm:"not null;unique"`
-	Email       string    `json: "email" gorm:"notnull;unique"`
-	Fullname    string    `json: "fullname" gorm: "not null"`
-	Pass        string    `json: "pass,omitempty" gorm:"not null;type:varchar(256)"`
-	ConfirmPass string    `json: "confirmPass,omitempty" gorm:"-"`
-	Picture     string    `json: "picture"`
+	Userame     string    `json:"username" gorm:"not null;unique"`
+	Email       string    `json:"email" gorm:"notnull;unique"`
+	Fullname    string    `json:"fullname" gorm:"not null"`
+	Pass        string    `json:"pass,omitempty" gorm:"not null;type:varchar(256)"`
+	ConfirmPass string    `json:"confirmPass,omitempty" gorm:"-"`
+	Picture     string    `json:"picture"`
 	Comments    []Comment `json:"comments,omitempty"`
 }
